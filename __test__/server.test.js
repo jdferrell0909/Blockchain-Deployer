1fab18c655e0f0f6b5901afc476a01d71efba9c3
James Ferrell <jamesferrell@jamess-mbp.lan>
2021-04-06 16:39:33 -0400

    finished building out server-side testing via supertest

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,33 +1,73 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const server = 'http://localhost:3000';
+const server = "http://localhost:3000";
 
-describe('/blockchain', () => {
-  describe('GET', () => {
-    it('responds with 200 status', () => {
+describe("/blockchain", () => {
+  describe("GET", () => {
+    it("responds with 200 status and correct content-type", () => {
       return request(server)
-        .get('/blockchain')
+        .get("/blockchain")
         .expect(200)
-    })
-  })
-})
+        .expect('Content-Type', /application\/json/)
+    });
 
-describe('/transaction', () => {
-  describe('POST', () => {
-    it('responds with 201 status', () => {
+    it('responds with an instance of a blockchain', () => {
       return request(server)
-        .post('/transaction')
+        .get("/blockchain")
+        .then(res => {
+          expect(typeof res.body).toBe('object');
+          expect(res.body).toHaveProperty('chain');
+          expect(res.body).toHaveProperty('pendingTransactions');
+        })
+    })
+  });
+});
+
+describe("/transaction", () => {
+  describe("POST", () => {
+    const body = {
+      amount: 100,
+      sender: "SENDERTEST",
+      recipient: "RECIPIENTTEST",
+    };
+
+    it("responds with 201 status and have correct content-type", () => {
+      return request(server)
+        .post("/transaction")
         .expect(201)
-    })
-  })
-})
+        .expect('Content-Type', /application\/json/)
+    });
 
-describe('/mine', () => {
-  describe('GET', () => {
-    it('responds with 200 status', () => {
+    it('should respond with an object with a note property', () => {
       return request(server)
-        .get('/mine')
-        .expect(200)
+        .post("/transaction")
+        .send(body)
+        .then(res => {
+          expect(res.body).toHaveProperty('note');
+        })
     })
-  })
-})
+  });
+});
+
+describe("/mine", () => {
+  describe("GET", () => {
+    it("responds with 200 status and correct content-type", () => {
+      return request(server)
+        .get("/mine")
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    });
+
+    it('responds with an object', () => {
+      return request(server)
+        .get("/mine")
+        .then(res => {
+          expect(typeof res.body).toBe('object');
+          expect(res.body).toHaveProperty('note');
+          expect(res.body).toHaveProperty('block');
+          expect(typeof res.body.block).toBe('object');
+          expect(res.body.block).toHaveProperty('index');
+        })
+    })
+  });
+});
